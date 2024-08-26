cb328daf8b1e12d5e7f6ccbd660e85f079b9cdf7
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-08-26 12:46:07 +0600

    For each first question solved

---
+++ script.js
@@ -0,0 +1,7 @@
+// array - forEach, map, filter
+// 1. Array er prottekti value er age hello print koro.
+var arr = ["hunny", "bunny", "popat"];
+
+arr.forEach(function(value){
+    console.log("Hello," + value);
+})
