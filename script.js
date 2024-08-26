073ffac2b6dec11d30ba8a09053c25f89b3015a6
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-08-26 12:51:58 +0600

    Problem number two solved

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // array - forEach, map, filter
 // 1. Array er prottekti value er age hello print koro.
-var arr = ["hunny", "bunny", "popat"];
+function problemOne(){
+    var arr = ["hunny", "bunny", "popat"];
 
 arr.forEach(function(value){
-    console.log("Hello," + value);
+    console.log("Hello, " + value);
 })
+}
+
+// 2. Array er protita value er moddhe jegula 2 er soman ar choto sudhu seguloi print koro with forEach
+function problemTwo(){
+    var arr2 = [1, 2, 3, 5, 6];
+arr2.forEach(function(value){
+    if(value<=2){
+        console.log(value)
+    }
+})
+}
